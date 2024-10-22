247b59aaa0e65ffcd60128bc5b65cb886eba2963
Tturna <>
2024-10-22 16:37:29 +0300

    prompt summary response

--- core/core.py
+++ core/core.py
@@ -1,5 +1,7 @@
+from typing import Any, Dict, Optional
 from flask import Flask, make_response, request, abort
 from dotenv import load_dotenv
+import datetime
 import utils.errorhandler as errorhandler
 from utils.commands import Command
 import services.weather as weather
@@ -62,6 +64,7 @@ def music_service_index():
 @app.route("/api/handle-prompt")
 def handle_prompt():
     prompt = request.args.get("p", None)
+    print(f"prompt: {prompt}")
 
     if prompt is None:
         response = make_response({ "error": "Prompt missing" })
@@ -77,6 +80,9 @@ def handle_prompt():
 
     print(f"command: {command.name} ({command.value})")
 
+    data: Optional[Dict[str, Any]] = None
+    status_code: int = 500
+
     match command:
         case Command.WEATHER_CURRENT:
             print("Handling current weather command...")
@@ -85,11 +91,6 @@ def handle_prompt():
             if errorhandler.check_service_error(data, status_code):
                 return errorhandler.get_service_error_response(data, status_code)
 
-            if data is None:
-                abort(status_code)
-
-            return data
-
         case Command.WEATHER_FORECAST:
             print("Handling weather forecast command...")
             status_code, data = weather.get_weather_forecast("Helsinki")
@@ -97,12 +98,40 @@ def handle_prompt():
             if errorhandler.check_service_error(data, status_code):
                 return errorhandler.get_service_error_response(data, status_code)
 
-            if data is None:
-                abort(status_code)
-
-            return data
-
         case Command.WEB_SEARCH:
             print("Handling web search command...")
 
-    return f"Command: {command.name}"
+    if data is None:
+        abort(status_code)
+
+    #summary_prompt = ("Give a very short summary of the following response data based on a prompt given by the user. "
+    #                  "Only summarize data that is relevant to the user prompt. "
+    #                  "Assume that the response data is always accurate. "
+    #                  "Answer with only the summary itself. "
+    #                  "Example user prompt: 'Is it hot today?', Good answer: 'No. The temperature is about "
+    #                  "14 Celsius.'. Example user prompt: 'How many legs do ants have?', Good answer: "
+    #                  "'Ants have 6 legs.'. "
+    #                  f"Your turn. The user said: {prompt}. "
+    #                  f"The response data: {data}")
+
+    #summary_prompt = ("A user enters a prompt and expects a short answer fast. "
+    #                  "The user might ask something or give a direct command."
+    #                  "You must use the given data to answer as concisely as possible. "
+    #                  "Assume that the given data is always accurate. Respond only with "
+    #                  "the answer itself. Ignore any data that is irrelevant to the user's query. "
+    #                  "Start immediately by answering the prompt without hesitation. "
+    #                  "Do not reply to me. Only respond to the user prompt. "
+    #                  "Example user prompt: 'Is it hot today?'. Example data: { weather: { "
+    #                  "avg_temp_c: 15, min_temp_c: 11, max_temp_c: 16, chance_of_rain: 0, cloud_coverage: 35, "
+    #                  "sunrise: 7am, sunset: 8pm } }. "
+    #                  "You would answer: 'The temperature is about 16 degrees Celsius today.' (ignore the rest of the data like rain, cloud coverage and sun visibility times). "
+    #                  "Example user prompt: 'How many legs do ants have?'. Example data: "
+    #                  "'**Answer:** Ants, like all insects, have six legs.\n\nAccording to various sources [1][2][3][4], the number of legs in ants is a consistent characteristic among all ant species. Whether an ant is a worker, queen, or drone, it will always have six legs, which are attached to their thorax (the middle section of their body).\n\nThese six legs are articulated, clawed, and sensitive [5][6], allowing ants to move quickly and efficiently, forage, communicate, defend, and build nests.\n\nIn fact, the number of legs in ants is determined by their genetics, just like other insects [7]. So, if you're wondering how many legs an ant has, the answer is always six!\n\nReferences:\n[1] - Context result 1\n[2] - Context result 2\n[3] - Context result 3\n[4] - Context result 4\n[5] - Context result 6\n[6] - Context result 8\n[7] - Context result 5'. "
+    #                  "You would answer: 'Ants have six legs.' (be super concise). Your turn. "
+    #                  f"User prompt: '{prompt}'. Data: {data}. Answer:")
+
+    summary_prompt = (f"Give a short one sentence answer to the user prompt '{prompt}' "
+                      f"based on this data: '{data} Current time: {datetime.datetime.now()}'. "
+                      "Answer with one short sentence that includes relevant details that a human might want to know.")
+
+    return ollama.send_prompt(summary_prompt)
--- core/services/ollama.py
+++ core/services/ollama.py
@@ -2,34 +2,41 @@ from typing import Optional
 import requests
 from utils.commands import Command
 
+PROMPT_URL = "http://localhost:11434/api/generate"
+DEFAULT_MODEL = "llama3:latest"
+
+def send_prompt(prompt: str) -> str:
+    body = {
+        "model": DEFAULT_MODEL,
+        "stream": False,
+        "prompt": prompt
+    }
+
+    response = requests.post(PROMPT_URL, json = body)
+    response_json = response.json()
+    return response_json["response"]
+
 def get_command_from_prompt(prompt: str) -> Optional[Command]:
     commands_string = ""
 
     for command in Command:
         commands_string += f"{command.value}. {command.name} "
 
-    body = {
-        "model": "llama3:latest",
-        "stream": False,
-        "prompt": (f"Answer with only a number. Which of the given commands is the best"
-                f"match to the given prompt? The prompt might not directly reference a command."
-                f"If none seem to match, resort to web search."
-                f"Commands: {commands_string} Example prompt: Will it rain today? Matching"
-                f"command: {Command.WEATHER_FORECAST.value}. Example prompt: How many legs"
-                f"do ants have? Matching command: {Command.WEB_SEARCH.value}. Example prompt:"
-                f"Is it cold tomorrow? Matching command: {Command.WEATHER_FORECAST.value}."
-                f"Example prompt: what's the temperature. Matching command: {Command.WEATHER_CURRENT.value}"
-                f"Example prompt: Nothing really matters. Resort to web search: "
-                f"{Command.WEB_SEARCH.value}. Your turn. Prompt: {prompt}")
-    }
+    prompt_string = (f"Answer with only a number. Which of the given commands is the best"
+                     f"match to the given prompt? The prompt might not directly reference a command."
+                     f"If none seem to match, resort to web search."
+                     f"Commands: {commands_string} Example prompt: Will it rain today? Matching"
+                     f"command: {Command.WEATHER_FORECAST.value}. Example prompt: How many legs"
+                     f"do ants have? Matching command: {Command.WEB_SEARCH.value}. Example prompt:"
+                     f"Is it cold tomorrow? Matching command: {Command.WEATHER_FORECAST.value}."
+                     f"Example prompt: what's the temperature. Matching command: {Command.WEATHER_CURRENT.value}"
+                     f"Example prompt: Nothing really matters. Resort to web search: "
+                     f"{Command.WEB_SEARCH.value}. Your turn. Prompt: {prompt}")
 
-    response = requests.post("http://localhost:11434/api/generate", json = body)
-    response_json = response.json()
-
-    command_number_string = str(response_json['response'])
+    command_number_string: str = send_prompt(prompt_string)
     command_number = int(command_number_string.split()[0])
 
-    if command_number > len(Command):
+    if command_number == 0 or command_number > len(Command):
         return None
     
     for command in Command:
